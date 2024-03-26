9bba26cb617c9a72ace872714c5d25ec7f7d30ed
kamilb13 <kamil.brzoza13@gmail.com>
2024-03-26 10:40:55 +0100

    Better layout and css

--- src/main/java/com/example/pierwszaaplikacja/WeatherApplication.java
+++ src/main/java/com/example/pierwszaaplikacja/WeatherApplication.java
@@ -10,8 +10,12 @@ import java.io.IOException;
 public class WeatherApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        //String cssFile = getClass().getResource("/styles.css").toExternalForm();
+
         FXMLLoader fxmlLoader = new FXMLLoader(WeatherApplication.class.getResource("weather.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 820, 440);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
         stage.setTitle("Weather App");
         stage.setScene(scene);
 
